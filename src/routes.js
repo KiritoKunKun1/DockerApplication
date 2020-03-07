eb5c93fb42523d4e1ef69a6faeea2a159c091e90
unknown <phsilvar@edu.univali.br>
2020-03-07 03:44:37 -0300

    Add Sequelize and User creation to postgres

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
+routes.get('/', async (req, res) => {
+	const user = await User.create({
+		name: 'Pedro Henrique',
+		email: 'pedrolol73@gmail.com.br',
+		password_hash: '1238712387'
+	});
+
+  return res.json({user});
 });
 
 export default routes;
